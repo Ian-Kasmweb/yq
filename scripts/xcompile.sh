154d88b8689d416bc00850eb709ed2f199a8039e
Ian Tangney <ian.tangney@kasmweb.com>
2024-09-12 19:11:01 +0000

    Remove man pages

--- scripts/xcompile.sh
+++ scripts/xcompile.sh
@@ -15,16 +15,12 @@ cd build
 # Remove artifacts from goreleaser
 rm artifacts.json config.yaml metadata.json
 
-find . -executable -type f | xargs -I {} tar czvf {}.tar.gz {} yq.1 -C ../scripts install-man-page.sh
-tar czvf yq_man_page_only.tar.gz yq.1 -C ../scripts install-man-page.sh
-
 rm yq_windows_386.exe.tar.gz
 rm yq_windows_amd64.exe.tar.gz
 
 zip yq_windows_386.zip yq_windows_386.exe
 zip yq_windows_amd64.zip yq_windows_amd64.exe
 
-rm yq.1
 
 rhash -r -a . -o checksums
 
